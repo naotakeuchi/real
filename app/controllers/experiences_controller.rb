1d5e569de8402a01ac40ef4df1c770834a6a1e2b
竹内菜緒 <Nao@takeuchinao-no-MacBook-Air.local>
2016-12-12 10:01:43 +0900

    コミットテスト

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -6,4 +6,15 @@ class ExperiencesController < ApplicationController
 
     def new
     end
+
+    def create
+      # binding.pryデーターがテーブルに反映されていなかったので確認
+      # Experience.create(genre: params[:genre], condition: params[:condition], treatment: params[:treatment], comment: params[:comment], detail: params[:detail])
+      Experience.create(experience_params)
+    end
+
+    private#指定したキーを持つパラメーターのみを受け取るようにするものです。もし仮に悪意のあるユーザーが、不正な情報を送信しようとしたときに、ストロングパラメーターを設定しておくと、不正な情報を受け取らずにすむ。
+    def experience_params
+      params.permit(:genre, :condition, :treatment, :comment, :detail)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
   Rails.application.routes.draw do
 
-    get 'experiences' => 'experiences#index'   #経験情報取得
-    get 'experiences/new'=>'experiences#new'   #新規サポーター会員登録画面
+    # get 'experiences' => 'experiences#index'   #経験情報表示
+    # get 'experiences/new'=>'experiences#new'   #新規サポーター会員登録画面
+    #→resourcesに切替
 
+    root 'experiences#index'
+    resources :experiences   #experiences_controllerに対してのresourcesメソッド
   end
 
